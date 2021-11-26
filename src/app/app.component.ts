03ab44c5911864bde59b30ba5a4f535f5fa620cf
Jayesh354 <jayesh.rohit@slscorp.com>
2021-11-26 12:24:52 +0530

    Added: Initial blueprint/structure for project is created
    Signed-off-by: Jayesh354 <jayesh.rohit@slscorp.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-complete-guidecource-project';
+  title = 'Recipe Shop';
 }
